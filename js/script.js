aa0244809a055a62b9136ada02cba022603b6b89
kadirula <kadirula17@gmail.com>
2023-12-30 10:25:36 +0300

    subtask dynamic element completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const subtasks = [];
+
+
 /******************* THEME DARK / LIGHT *********************/
 const theme = localStorage.getItem('theme') || 'light';
 const body = document.body;
@@ -48,6 +51,87 @@ kanbanDropItems.forEach(dropItem => {
 
 /******************* end:: KANBAN | DRAG & DROP *********************/
 
+/******************* begin:: CREATE TASK *********************/
+const taskForm = document.getElementById('task-form');
+
+if(taskForm){
+    taskForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(taskForm);
+        const formDataObject = {};
+
+        // FormData nesnesinin her bir öğesini döngü ile işle
+        formData.forEach((value, key) => {
+            // Eğer aynı isme sahip birden fazla input varsa, değerleri bir dizi içinde tut
+            if (formDataObject.hasOwnProperty(key)) {
+            // Eğer değer daha önce bir dizi içine eklenmediyse, bir dizi oluştur
+            if (!Array.isArray(formDataObject[key])) {
+                formDataObject[key] = [formDataObject[key]];
+            }
+            // Değeri diziye ekle
+            formDataObject[key].push(value);
+            } else {
+            // Aynı isme sahip başka bir input yoksa, değeri doğrudan ata
+            formDataObject[key] = value;
+            }
+        });
+
+    })
+}
+
+/******************* end:: CREATE TASK *********************/
+
+
+/******************* begin:: DYNAMIC SUBTASK *********************/
+const subtask = document.getElementById('subtask');
+if(subtask){
+    const defaultTaskCount = 2;
+    for (let index = 0; index < defaultTaskCount; index++) {
+
+        const elementCount = index + 1;
+        const taskElementItem = {
+            index: index,
+            name: 'subtask',
+            title: `Sub Task - ${elementCount}`
+        };
+
+        subtasks.push(taskElementItem);
+    }
+
+    subtasks.map((item, index) => {
+        const subTaskElement = {
+            index: index,
+            name: 'subtask'
+        };
+
+        renderSubTask(subTaskElement);
+    });
+
+    
+   
+}
+
+
+const addNewTaskBtn = document.getElementById('add-new-task');
+
+if(addNewTaskBtn){
+    addNewTaskBtn.addEventListener('click', () => {
+
+        const subTaskElement = {
+            index: subtasks.length,
+            name: 'subtask'
+        };
+
+        console.log("subTaskElement", subTaskElement);
+
+        renderSubTask(subTaskElement);
+
+    })
+}
+
+/******************* end:: DYNAMIC SUBTASK *********************/
+
 
 /******************* begin:: FUNCTIONS *********************/
 function removeClassElements(elements, className){
@@ -55,4 +139,21 @@ function removeClassElements(elements, className){
         element.classList.remove(className);
     })
 }
+
+function renderSubTask(item){
+
+    const subTaskElement = `
+            <div class="input-group mb-3" id="subtask-${item.index}">
+                <input type="text" class="form-control" placeholder="Subtasks..." name="${item.name}" />
+                <span class="input-action">
+                    <i class="fa-solid fa-xmark"></i>
+                </span>
+            </div>
+        `;
+   
+
+    subtask.innerHTML += subTaskElement;
+}
 /******************* end:: FUNCTIONS *********************/
+
+

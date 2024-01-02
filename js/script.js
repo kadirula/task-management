33339958fc8fbd100f7461a95bd84d986a00f902
kadirula <kadirula17@gmail.com>
2024-01-02 09:27:37 +0300

    kanban card add operation completed

--- js/script.js
+++ js/script.js
@@ -1,26 +1,34 @@
 const subtasks = [];
-const kanbanItems = [];
+const kanbanItems = [
+    {
+        id: 1,
+        name: 'Todo'
+    },
+    {
+        id: 2,
+        name: 'Doing'
+    },
+    {
+        id: 3,
+        name: 'Done'
+    },
+
+];
+
+
 const users = [
     {
         id: 1,
         name: 'User - 1',
         kanban: [
             {
+                id: 1,
                 title: 'Todo',
                 cards: [
                     {
                         head: 'Card Title - 1',
                         description: '',
-                        subtasks: [
-                            {
-                                id: 1,
-                                name: 'Subtask - 1'
-                            },
-                            {
-                                id: 2,
-                                name: 'Subtask - 2'
-                            }
-                        ]
+                        subtasks: ["Subtask - 1", "Subtask - 2"]
                     }
                 ]
             }
@@ -31,17 +39,13 @@ const users = [
         name: 'User - 2',
         kanban: [
             {
+                id: 2,
                 title: 'Doing',
                 cards: [
                     {
                         head: 'Card Title - 1',
                         description: '',
-                        subtasks: [
-                            {
-                                id: 1,
-                                name: ''
-                            }
-                        ]
+                        subtasks: ["Subtask - 1", "Subtask - 2"]
                     }
                 ]
             }
@@ -60,73 +64,11 @@ body.classList.add(`theme-${theme}`);
 
 
 /******************* begin:: USERS  *********************/
-const userListItem = document.querySelector('.user-tab__list');
-if(userListItem){
-
-    users.map((user_item, index) => {
-        const itemIndex = index + 1;
-        const item = `
-        <li class="user-tab__list-item ${itemIndex == 1 && 'active'}" data-tab="user-${itemIndex}">
-        <i class="fa-regular fa-user"></i>
-        <span>${user_item.name}</span>
-        </li>`;
-        
-        userListItem.innerHTML += item;
-    });
-}
+renderUser();
 /******************* end:: USERS  *********************/
 
 /******************* begin:: USER CONTENT  *********************/
-const userTabContent = document.querySelector('.user-tab__content');
-if(userTabContent){
-
-    users.map((user_item, index) => {
-        const itemIndex = index + 1;
-        const item = `
-        <div class="user-tab__content-item ${itemIndex == 1 && 'show'}" id="user-${itemIndex}">
-            <div class="kanban"></div>
-        </div>`;
-
-        userTabContent.innerHTML += item;
-
-
-        const userTabKanban = document.querySelector(`#user-${itemIndex} .kanban`);
-
-        if(userTabKanban){
-            const kanbans = user_item.kanban;
-            kanbans.map(kanban_item => {
-                userTabKanban.innerHTML += `
-                    <div class="kanban__item">
-                        <h4 class="kanban__title">${kanban_item.title}</h4>
-                        <div class="kanban__content"></div>
-                    </div>
-                `;
-
-                const userKanbanContent = userTabKanban.querySelector('.kanban__content');
-
-                if(userKanbanContent){
-                    const taskCards = kanban_item.cards;
-                    taskCards.map(task_item => {
-
-                        userKanbanContent.innerHTML += `
-                        <div class="task-card">
-                            <div class="task-card__head">${task_item.head}</div>
-                            <div class="task-card__body">
-                                0 of ${task_item.subtasks.length} subtasks
-                            </div>
-                        </div>
-                        `;
-                    })
-                }
-            })
-        }
-    });
-
-
-    
-
-
-}
+renderUserContent();
 /******************* end:: USER CONTENT  *********************/
 
 
@@ -156,19 +98,7 @@ userTabListItems.forEach(tabItem => {
 /******************* end:: USER TAB *********************/
 
 /******************* begin:: KANBAN | DRAG & DROP *********************/
-const kanbanDropItems = document.querySelectorAll('.kanban__content');
-
-kanbanDropItems.forEach(dropItem => {
-    if(dropItem){
-        new Sortable(dropItem, {
-            group: 'kanban',
-            animation: 150,
-            onAdd: function (evt) {
-                console.log('Added:', evt);
-            }
-        })
-    }
-})
+kanbanDragDrop();
 
 /******************* end:: KANBAN | DRAG & DROP *********************/
 
@@ -178,6 +108,7 @@ const taskForm = document.getElementById('task-form');
 if(taskForm){
     taskForm.addEventListener('submit', (e) => {
         e.preventDefault();
+
 
         const formData = new FormData(taskForm);
         const formDataObject = {};
@@ -197,6 +128,35 @@ if(taskForm){
             formDataObject[key] = value;
             }
         });
+
+
+
+        const findUser = users.find(user => user.id == 1);
+
+        if(findUser){
+            const userKanban = findUser.kanban.find(item => item.title == formDataObject.status);
+            if(userKanban){
+                const card = {
+                    head: formDataObject.title,
+                    description: formDataObject.description,
+                    subtasks: formDataObject.subtask
+                };
+                userKanban.cards.push(card);
+
+            }
+            else{
+
+            }
+            
+            renderUserContent();
+        }
+        else{
+
+        }
+
+        console.log(formDataObject);
+
+        
 
     })
 }
@@ -244,14 +204,24 @@ if(addNewTaskBtn){
             name: 'subtask'
         };
 
-        console.log("subTaskElement", subTaskElement);
-
         renderSubTask(subTaskElement);
 
     })
 }
 
 /******************* end:: DYNAMIC SUBTASK *********************/
+
+/******************* begin:: SELECT KANBAN ITEMS *********************/
+const kanbanSelect = document.getElementById('kanban-item-select');
+if(kanbanSelect){
+    kanbanItems.map(kanban_item => {
+        const selectItem = `
+        <option value="${kanban_item.name}">${kanban_item.name}</option>`;
+
+        kanbanSelect.innerHTML += selectItem;
+    })
+}
+/******************* end:: SELECT KANBAN ITEMS *********************/
 
 
 /******************* begin:: FUNCTIONS *********************/
@@ -274,6 +244,99 @@ function renderSubTask(item){
    
 
     subtask.innerHTML += subTaskElement;
+}
+
+function renderUser(){
+    const userListItem = document.querySelector('.user-tab__list');
+    if(userListItem){
+    
+        users.map((user_item, index) => {
+            const itemIndex = index + 1;
+            const item = `
+            <li class="user-tab__list-item ${itemIndex == 1 && 'active'}" data-tab="user-${itemIndex}">
+            <i class="fa-regular fa-user"></i>
+            <span>${user_item.name}</span>
+            </li>`;
+            
+            userListItem.innerHTML += item;
+        });
+    }
+}
+
+function renderUserContent(){
+
+    const userTabContent = document.querySelector('.user-tab__content');
+    if(userTabContent){
+
+        users.forEach((user_item, index) => {
+            const itemIndex = index + 1;
+            const item = `
+            <div class="user-tab__content-item ${itemIndex == 1 && 'show'}" id="user-${itemIndex}">
+                <div class="kanban"></div>
+            </div>`;
+
+            userTabContent.innerHTML += item;
+
+
+            const userTabKanban = document.querySelector(`#user-${itemIndex} .kanban`);
+
+            if(userTabKanban){
+                const kanbans = user_item.kanban;
+
+                userTabKanban.innerHTML = '';
+
+
+                kanbans.forEach(kanban_item => {
+
+                    userTabKanban.innerHTML += `
+                        <div class="kanban__item">
+                            <h4 class="kanban__title">${kanban_item.title}</h4>
+                            <div class="kanban__content"></div>
+                        </div>
+                    `;
+
+                    const userKanbanContent = userTabKanban.querySelector('.kanban__content');
+
+                    if(userKanbanContent){
+                        userKanbanContent.innerHTML = '';
+                        const taskCards = kanban_item.cards;
+
+                        console.log("12312312312", taskCards);
+
+                        taskCards.forEach(task_item => {
+
+                            userKanbanContent.innerHTML += `
+                            <div class="task-card">
+                                <div class="task-card__head">${task_item.head}</div>
+                                <div class="task-card__body">
+                                    0 of ${task_item.subtasks?.length} subtasks
+                                </div>
+                            </div>
+                            `;
+                        })
+                    }
+                })
+            }
+        });
+
+        kanbanDragDrop();
+    }
+}
+
+function kanbanDragDrop(){
+    const kanbanDropItems = document.querySelectorAll('.kanban__content');
+
+    kanbanDropItems.forEach(dropItem => {
+        if(dropItem){
+            new Sortable(dropItem, {
+                group: 'kanban',
+                animation: 150,
+                onAdd: function (evt) {
+                    console.log('Added:', evt);
+                }
+            })
+        }
+    })
 }
 /******************* end:: FUNCTIONS *********************/
 
